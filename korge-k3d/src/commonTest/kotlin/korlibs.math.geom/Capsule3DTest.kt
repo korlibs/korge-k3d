44974c6eb1f53260da0753e9420c4cdebfb158e4
soywiz <soywiz@gmail.com>
2023-06-06 21:19:52 +0200

    Small cleanup

--- korge-k3d/src/commonTest/kotlin/korlibs.math.geom/Capsule3DTest.kt
+++ korge-k3d/src/commonTest/kotlin/korlibs.math.geom/Capsule3DTest.kt
@@ -6,9 +6,10 @@ class Capsule3DTest {
     @Test
     fun test() {
         val c1 = Capsule3D(Vector3.ZERO, Vector3.UP, 1f)
-        val c2 = Capsule3D(Vector3.ZERO + Vector3(3f, 0f, 0f), Vector3.UP + Vector3(3f, 0f, 0f), 1f)
-        val se2 = Segment3D(Vector3.ZERO + Vector3(3f, 0f, 0f), Vector3.UP + Vector3(3f, 0f, 0f))
-        val s2 = Sphere3D(Vector3.ZERO + Vector3(3f, 0f, 0f), 1f)
+        val offset = Vector3(3f, 0f, 0f)
+        val c2 = Capsule3D(Vector3.ZERO + offset, Vector3.UP + offset, 1f)
+        val se2 = Segment3D(Vector3.ZERO + offset, Vector3.UP + offset)
+        val s2 = Sphere3D(Vector3.ZERO + offset, 1f)
         assertEquals(1f, c1.distanceTo(c2), 0.0001f)
         assertEquals(1f, c1.distanceTo(s2), 0.0001f)
         assertEquals(2f, c1.distanceTo(se2), 0.0001f)
